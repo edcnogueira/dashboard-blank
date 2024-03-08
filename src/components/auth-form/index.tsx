77501d3bf1431a3103b1e9cfda4a3b6f279679ac
Edjancarlos <edjan.nogueira14@gmail.com>
2024-03-08 00:06:24 -0300

    feat: add react-hook-form and zod

--- src/components/auth-form/index.tsx
+++ src/components/auth-form/index.tsx
@@ -1,21 +1,41 @@
+/* eslint-disable prettier/prettier */
 'use client'
 
 import * as React from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
 import { Icons } from '@/utils/icons'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import {
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormLabel,
+   FormMessage,
+} from '../ui/form'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+const formSchema = z.object({
+  email: z.string().min(1, { message: 'Email required' }).email(),
+})
+
+type TFormSchema = z.infer<typeof formSchema>
+
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const form = useForm<TFormSchema>({
+    resolver: zodResolver(formSchema),
+  })
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
+  async function onSubmit(data: TFormSchema) {
     setIsLoading(true)
+    console.log(data)
 
     setTimeout(() => {
       setIsLoading(false)
@@ -23,31 +43,43 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   }
 
   return (
-    <div className={cn('grid gap-6', className)} {...props}>
-      <form onSubmit={onSubmit}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-          </div>
+    <Form {...form}>
+      <div className={cn('grid gap-6', className)} {...props}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-2"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    placeholder="name@example.com"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
           <Button disabled={isLoading}>
             {isLoading && (
               <Icons.spinner className="mr-2 size-4 animate-spin" />
             )}
             Sign In with Email
           </Button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </Form>
   )
 }
