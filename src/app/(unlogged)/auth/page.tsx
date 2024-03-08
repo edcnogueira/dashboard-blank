a7016f6e9b7b61acb1887b56937143be2195882f
Edjancarlos <edjan.nogueira14@gmail.com>
2024-03-08 00:08:08 -0300

    fix: responsivite

--- src/app/(unlogged)/auth/page.tsx
+++ src/app/(unlogged)/auth/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container grid h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
